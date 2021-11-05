53fa79322c917648b5aa313cbbc95c2130e9cb26
rizqitsani <rizqitsani@gmail.com>
2021-11-05 15:20:36 +0700

    feat: add basic seo

---
+++ next-seo.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+const DEFAULT_SEO = {
+  title: undefined,
+  titleTemplate: '%s | Next.js + Chakra UI + Typescript Starter',
+  defaultTitle: 'Next.js + Chakra UI + Typescript Starter',
+  description:
+    'A personal opinionated starter for Next.js, Chakra UI, and Typescript',
+  canonical: 'https://next-chakra-ts-starter.vercel.app/',
+  openGraph: {
+    url: 'https://next-chakra-ts-starter.vercel.app/',
+    type: 'website',
+    title: `Next.js + Chakra UI + Typescript Starter`,
+    description: `A personal opinionated starter for Next.js, Chakra UI, and Typescript`,
+    site_name: 'Next.js + Chakra UI + Typescript Starter',
+  },
+};
+
+export default DEFAULT_SEO;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { DefaultSeo } from 'next-seo';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import theme from '@/theme';
 
+import DEFAULT_SEO from '../../next-seo.config.js';
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <ChakraProvider theme={theme}>
+      <DefaultSeo
+        {...DEFAULT_SEO}
+        canonical={`https://next-chakra-ts-starter.vercel.app${router.asPath}`}
+        openGraph={{
+          url: `https://next-chakra-ts-starter.vercel.app${router.asPath}`,
+        }}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   );
